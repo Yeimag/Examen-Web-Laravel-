2590ff9807bcfe3d1e354c52cc9f021779e872c6
Yeimag <marielaag.15@gmail.com>
2017-06-04 00:27:05 -0600

    actual

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.css"  rel="stylesheet">
+<nav>
+    <div class="nav-wrapper">
+        <a href="" class="brand-logo">Logo</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="{{ url('/Inventario') }}">Inventario</a></li>
+            <li><a href="{{ url('/productos') }}">Productos </a></li>
+            <li><a href="{{ url('/proveedores') }}">Proveedores</a></li>
+            <li><a href="{{ url('/MovimientosInventario') }}">Movimientos Inventario</a></li>
+        </ul>
     </div>
-</div>
+</nav>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Examen Web</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -66,22 +66,6 @@
         </style>
     </head>
     <body>
-
-        <nav>
-          <div class="nav-wrapper">
-            <a href="" class="brand-logo">Logo</a>
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
-              <li><a href="{{ url('/Inventario') }}">Inventario</a></li>
-              <li><a href="{{ url('/productos') }}">Productos </a></li>
-              <li><a href="{{ url('/proveedores') }}">Proveedores</a></li>
-              <li><a href="{{ url('/MovimientosInventario') }}">Movimientos Inventario</a></li>
-            </ul>
-          </div>
-        </nav>
-
-
-
-
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -96,15 +80,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Examen Web Laravel
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div >
+                    <h4>Yerlin Ávila Gómez</h4>
+                    <h4>Andrés García Salas</h4>
                 </div>
             </div>
         </div>
