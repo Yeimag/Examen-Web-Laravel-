24911e81098f02b9656ac32a6953228bfdd6633d
Andrés García <21salchi21@gmail.com>
2017-06-06 00:46:37 -0600

    merge solve

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -2,8 +2,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Cliente;
-use examenweblaravel;
+use App\Clientes;
+
 class ClienteController extends Controller {
 
     /*public function __construct()
